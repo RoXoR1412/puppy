c6fea491b4019d72f4a38149a85042153ef07556
Gaurav Chitlangia <gauravchitlangia.14@gmail.com>
2024-12-09 02:07:56 +0530

    dog

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/RoXoR1412/puppy
 
 go 1.23.3
+
+require github.com/RoXoR1412/dog v0.0.0-20241208202710-0d5ac507a14d // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,20 @@
 package puppy
 
+import (
+	"github.com/RoXoR1412/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrowsUp(Bark())
+}
+func BigBark() string {
+	return dog.WhenGrowsUp(Barks())
 }
